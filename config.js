c4514e87cd5636f3bc31c74b44ca8f209ba7c534
苹果派派 <i@imzcc.com>
2024-07-08 21:10:41 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 window.Config = {
 
   // 显示标题
-  SiteName: 'ZGQ Inc. の 站点监测',
+  SiteName: '水岸兰香 · 站点监测',
 
   // UptimeRobot API Keys
   // 支持Monitor-Specific API Keys和Read-Only API Key，推荐使用Read-Only API Key，可填写多个
   ApiKeys: [
-    'ur1884169-f2ddd4677953551305552673',
+    '5b6d58c0759e45b56203ff56',
   ],
 
   // 日志天数，建议90
@@ -18,16 +18,8 @@ window.Config = {
   // 导航栏菜单
   Navi: [
     {
-      text: '个人主页',
-      url: 'https://domain.zgqinc.gq/'
-    },
-    {
-      text: '项目地址',
-      url: 'https://github.com/ZGQ-inc/uptime-status'
-    },
-    {
-      text: '原项目地址',
-      url: 'https://github.com/imsyy/status'
+      text: '官方主页',
+      url: 'https://www.salx.cn'
     },
   ],
 };
